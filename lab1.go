dfcf5c2dfff4ab0711eb31ed363dfcbe741bf174
zanhui.fan <48016409+ZanhuiFan@users.noreply.github.com>
2024-01-25 11:29:44 +0800

    Update lab1.go

--- lab1.go
+++ lab1.go
@@ -16,3 +16,18 @@ func main() {
     runner.Benchmark = benchmark
     runner.Run()
 }
+
+type Runner struct {
+    engine            sim.Engine
+    GPUDriver         *driver.Driver
+    Benchmark         benchmarks.Benchmark
+}
+
+func (r *Runner) Init() {
+    r.engine, r.GPUDriver = platform.BuildNR9NanoPlatform(1)
+}
+
+func (r *Runner) Run() {
+    r.Benchmark.Run()
+    r.engine.Finished()
+}
